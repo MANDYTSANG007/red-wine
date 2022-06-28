a3f6f981478e7391644ddaebfdd355f75cdc0bf3
Mandy Tsang <mandy.tsang007@gmail.com>
2022-06-28 16:51:53 -0700

    adjust data flow mechanism

--- src/components/DataFlow.js
+++ src/components/DataFlow.js
@@ -7,116 +7,65 @@
 
 
 import {useState, useReducer, useRef} from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 import Header from './Header';
 import Body from './Body';
-import Dialog from './Dialog';
+// import Dialog from './Dialog';
 import ExcelF from './ExcelF';
 import Form from './Form';
 import clone from '../modules/clone';
+
+import schema from '../config/schema';
+import DataContext from '../contexts/DataContext';
+
+let initialData = JSON.parse(localStorage.getItem('data'));
+
+if (!initialData) {
+    initialData = [{}];
+    Object.keys(schema).forEach(
+        (key) => (initialData[0][key] = schema[key].samples[0]),
+    );
+}
 
 //use local storage in case the user closes the browser tab
 function commitToStorage(data) {
     localStorage.setItem('data', JSON.stringify(data));
 }
 
-//use reducer to in charge of two types of events(actions): save and excelchange
-//the potencial benefit of using a reducer is that it's simpler to expand on if future new actions are expected.
-function reducer(data, action){
-    //save will create a new record in data when the user clicks the +ADD WINE button
-    //Why is it necessary to clone the data b/4 adding to it(via array's unshift()? it's b/c the reducer
-    // is called twice in development and the same record would be added twice otherwise.)
-    if (action.type === 'save') {
-        data = clone(data);
-        data.unshift(action.payload.formData);
-        commitToStorage(data);
-        return data;
-    }
-    //excelchange handles any data change coming from ExcelF. This action doesn't modify the data, just commits 
-//it to storage and returns it as-is.
-    if (action.type === 'excelchange') {
-        commitToStorage(action.payload.updatedData);
-        return action.payload.updatedData;
-    }
-}
+//keep the data in the state
+function DataFlow() {
+    const [data, setData] = useState(initialData);
+    const [filter, setFilter] = useState(null);
 
-//use a reducer for data since it's potentially more involved and for everthing else, stick with state
-function DataFlow({ schema, initialData }) {
-    const [ data, dispatch ] = useReducer( reducer, initialData );
-    const [ addNew, setAddNew ] = useState(false);
-    const [ filter, setFilter ] = useState(null);
-
-    const form = useRef(null);
-
-    // saveNew() helper handles dialog actions.It closes the dialog unconditionally (by setting the addNew state)
-    // and if the dialog wasn't simply dismissed, it collects the form data from the dialog and dispatches the 
-    // appropriate save action for the reducer to handle.
-    function saveNew(action) {
-        setAddNew(false);
-        if (action === 'dismiss') {
-            return;
-        }
-
-        const formData = {};
-        Array.from(form.current).forEach(
-            (input) => (formData[input.id] = input.value),
-        );
-
-        dispatch({
-            type: 'save',
-            payload: {formData},
-        });
+    //update the data- this parent DataFlow receives the new data and updates the state
+    // with setData(), and this causes React to rerender. It takes 3 steps: 1)clone the data so it's always immutable 
+    //2)save it to localStorage for the next time
+    //the app is loaded. 3) update the state
+    function updateData(newData) {
+        newData = clone(newData);
+        commitToStorage(newData);
+        setData(newData);
     }
 
-    // onExcelDataChange is a cb that takes any data updates from ExcelF and dispatches an action to be handled
-    //by the reducer.
-    function onExcelDataChange(updatedData) {
-        dispatch({
-            type: 'excelchange',
-            payload: {updatedData},
-        });
-    }
-
-    //onSearch() takes the search string from the header and updates the filter state, which (by way
+     //onSearch() takes the search string from the header and updates the filter state, which (by way
     //of rerendering) is passed to ExcelF, where it's used to show only matching data records.
     function onSearch(e) {
-        setFilter(e.target.value);
+        const s = e.target.value;
+        setFilter(s);
     }
 
-     //combine all the main components
-     return (
+    //combine all the main components
+    return (
         <div className='DataFlow'>
-            <Header
-                onAdd={() => setAddNew(true)}
-                onSearch={onSearch}
-                count={data.length}
-            />
-            <Body>
-                <ExcelF
-                    schema={schema}
-                    initialData={data}
-                    key={data}
-                    onDataChange={(updatedData) => onExcelDataChange(updatedData)}
-                    filter={filter}
-                />
-                {addNew ? (
-                    <Dialog
-                        modal={true}
-                        header='Add new item'
-                        confirmLabel='Add'
-                        onAction={(action) => saveNew(action)}>
-                            <Form ref={form} fields={schema} />
-                    </Dialog>
-                ) : null}
-            </Body>
+            <DataContext.Provider value={{data, updateData}}>
+                <Header onSearch={onSearch} />
+                <Body>
+                    <ExcelF filter={filter} />
+                </Body>
+            </DataContext.Provider>
         </div>
-     );
+        );
 }
-
-DataFlow.propTypes= {
-    schema: PropTypes.object.isRequired,
-    initialData: PropTypes.arrayOf(PropTypes.object).isRequired,
-};
 
 export default DataFlow;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,73 @@
 import Logo from './Logo';
 import './Header.css';
+import {useContext, useState, useRef} from 'react';
+
 import Button from './Button';
 import FormInput from './FormInput';
+import Dialog from './Dialog';
+import Form from './Form';
+import schema from '../config/schema';
 
-// the header doesn't do any searching or adding to the data, but it offers callbacks for its
-//parent to do the data management
-function Header({ onSearch, onAdd, count = 0 }) {
+import DataContext from '../contexts/DataContext';
+
+// this header is responsible for the UI, the Form in a Dialog to add new records. That's why the list of imports is 
+//a little longer. The pieces of data needed to render the header are:  1) data coming from the context
+//2) addNew flag whether or not the Add dialog is shown(when the user clicks the Add button)
+//we use useContext() hook to get access to the value prop passed by the <DataContext.Provider>. It's an object
+//that has a data property and a functin updateData().
+//the header can access all of the data and get the count from (data.length). 
+//The helper functions here: onAdd() and saveNew(). The job of saveNew() is to gather the new record from the
+//form and add it to data. 
+// 
+function Header({ onSearch }) {
+    const {data, updateData} = useContext(DataContext);
+    const count = data.length;
+
+    const [addNew, setAddNew] = useState(false);
+
+    const form = useRef(null);
+
+    function saveNew(action) {
+        setAddNew(false);
+        if (action === 'dismiss') {
+            return;
+        }
+
+        const formData = {};
+        Array.from(form.current).forEach(
+            (input) => (formData[input.id] = input.value),
+        );
+        data.unshift(formData);
+        updateData(data);
+    }
+    
+    function onAdd() {
+        setAddNew(true);
+    }
+
     const placeholder = count > 1 ? `Search ${count} items` : 'Search';
     return (
-        <div className='Header'>
-            <Logo />
-            <div>
-                <FormInput placeholder={placeholder} id="search" onChange={onSearch} />
+        <div>
+            <div className='Header'>
+                <Logo />
+                <div>
+                    <FormInput placeholder={placeholder} id="search" onChange={onSearch} />
+                </div>
+                <div>
+                    <Button onClick={onAdd}> <b>&#65291;</b> Add wine </Button>
+                </div>
             </div>
-            <div>
-                <Button onClick={onAdd}> <b>&#65291;</b> Add wine </Button>
-            </div>
+            {addNew ? (
+                <Dialog
+                    modal={true}
+                    header="Add new item"
+                    confirmLabel="Add"
+                    onAction={( action ) => saveNew(action)}>
+                        <Form ref={form} fields={schema} />
+                </Dialog>
+            ) : null }
         </div>
     )
-};
+}
 
 export default Header;
