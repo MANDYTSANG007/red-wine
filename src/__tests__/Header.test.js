105053964cc8e719f91319903ed258b7bed6a06c
Mandy Tsang <mandy.tsang007@gmail.com>
2022-07-12 21:28:43 -0700

    add __tests__ folder

--- src/__tests__/Header.test.js
+++ src/__tests__/Header.test.js
@@ -1,6 +1,6 @@
 import {render, screen} from '@testing-library/react';
 import { toHaveClass } from '@testing-library/jest-dom';
-import Header from './Header';
+import Header from '../components/Header';
 
 test("should render a button with the class of primary", () => {
     render(<Header />)
