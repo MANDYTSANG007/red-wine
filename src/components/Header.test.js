3debaaf88107ff0882a2316a60e1b7714a6c25ff
Mandy Tsang <mandy.tsang007@gmail.com>
2022-07-12 19:47:06 -0700

    fix typo in unit test

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -2,7 +2,7 @@ import {render, screen} from '@testing-library/react';
 import { toHaveClass } from '@testing-library/jest-dom';
 import Header from './Header';
 
-it("should render a button with the class of primary", () => {
+test("should render a button with the class of primary", () => {
     render(<Header />)
     const button = screen.getByRole('button', { button: /primary/i })
     expect(button).toHaveClass('primary')
